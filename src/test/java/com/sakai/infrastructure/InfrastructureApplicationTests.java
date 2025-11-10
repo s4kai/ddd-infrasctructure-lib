00dbf1fdee2ae7b5d823364d5545ae83c6cb0fbf
Vinicius Sakai <vinicius.sakai@tuta.io>
2025-11-10 19:00:58 -0300

    chore: init repo

---
+++ src/test/java/com/sakai/infrastructure/InfrastructureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sakai.infrastructure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfrastructureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
