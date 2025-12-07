e3c66cae75750e966640044eecb948c040f15f3c
Vinicius Sakai <vinicius.sakai@tuta.io>
2025-12-07 03:03:07 -0300

    refactor: adjust to new interface of DomainEventPublisher

--- src/main/java/com/sakai/infrastructure/events/SpringDomainEventPublisher.java
+++ src/main/java/com/sakai/infrastructure/events/SpringDomainEventPublisher.java
@@ -1,13 +1,11 @@
 package com.sakai.infrastructure.events;
 
-import com.sakai.domain.entity.AggregateRoot;
 import com.sakai.domain.events.DomainEvent;
 import com.sakai.domain.events.DomainEventPublisher;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Component;
 
-import java.util.Collections;
 import java.util.List;
 
 @Component
@@ -21,13 +19,8 @@ public class SpringDomainEventPublisher implements DomainEventPublisher {
         publisher.publishEvent(domainEvent);
     }
 
-    public void publish(AggregateRoot<?> aggregateRoot) {
-        List<Object> events = Collections.singletonList(aggregateRoot.getDomainEvents());
-        for (Object event : events) {
-            publish((DomainEvent) event);
-        }
-        aggregateRoot.clearDomainEvents();
+    public void publishAllEvents(List<DomainEvent> events) {
+        events.forEach(this::publish);
     }
-
 }
 
