d8f5be15134e4a39effe62cb3a5272f8338b4c96
Vinicius Sakai <vinicius.sakai@tuta.io>
2025-12-08 01:55:53 -0300

    feat: add logs in SpringDomainEventPublisher

--- src/main/java/com/sakai/infrastructure/events/SpringDomainEventPublisher.java
+++ src/main/java/com/sakai/infrastructure/events/SpringDomainEventPublisher.java
@@ -2,6 +2,7 @@ package com.sakai.infrastructure.events;
 
 import com.sakai.domain.events.DomainEvent;
 import com.sakai.domain.events.DomainEventPublisher;
+import com.sakai.logging.DomainLogger;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Component;
@@ -11,15 +12,17 @@ import java.util.List;
 @Component
 @RequiredArgsConstructor
 public class SpringDomainEventPublisher implements DomainEventPublisher {
-
     private final ApplicationEventPublisher publisher;
+    private final DomainLogger logger = DomainLogger.get(SpringDomainEventPublisher.class);
 
     @Override
     public void publish(DomainEvent domainEvent) {
+        logger.trace("Evento publicado: {}", domainEvent);
         publisher.publishEvent(domainEvent);
     }
 
     public void publishAllEvents(List<DomainEvent> events) {
+        logger.trace("Publicando {} eventos", events.size());
         events.forEach(this::publish);
     }
 }
